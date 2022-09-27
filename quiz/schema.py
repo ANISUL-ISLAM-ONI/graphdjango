244dc8f2af2811f884e38b22616c939e71744872
ANISUL-ISLAM-ONI <aminjoy82@gmail.com>
2022-09-27 11:23:20 +0600

    CRUD Done

--- quiz/schema.py
+++ quiz/schema.py
@@ -34,6 +34,32 @@ class Query(graphene.ObjectType):
   def resolve_all_answers(root, info, id):
     return Answer.objects.filter(question=id)
 
-  
 
-schema = graphene.Schema(query=Query)
+
+# class CategoryMutation(graphene.Mutation):
+#   class Arguments:
+#     name = graphene.String(required=True)
+
+#   category = graphene.Field(CategoryType)
+
+#   @classmethod
+#   def mutate(cls, root, info, name):
+#     category = Category(name=name)
+#     category.save()
+#     return CategoryMutation(category=category)
+class CategoryMutation(graphene.Mutation):
+  class Arguments:
+    id = graphene.ID()
+
+  category = graphene.Field(CategoryType)
+
+  @classmethod
+  def mutate(cls, root, info, id):
+    category = Category.objects.get(id=id)
+    category.delete()
+    return
+
+class Mutation(graphene.ObjectType):
+  update_category = CategoryMutation.Field() 
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
